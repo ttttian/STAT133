a626711247e2e3ea888d8f7a6b4e3d81abe453b4
Lingtian Cheng <lingtian.cheng@gmail.com>
2015-01-29 23:45:09 -0800

    Minor style change

--- assignments/hw1/hw1.R
+++ assignments/hw1/hw1.R
@@ -41,12 +41,12 @@
 # << y >> : a vector of length 100 which has the even numbers from 2 to 200.
 # For clarity put your code here, directly below the 
 
-y <- seq(2, 200, by=2)
+y <- seq(2, 200, by = 2)
 
 # Create the variable
 # << z >> : a vector of length 20 with character entries, "hw1", "hw2", ..., "hw20"
 
-z <- paste("hw", 1:20, sep="")
+z <- paste("hw", 1:20, sep = "")
 
 # Create the variable
 # << m >> : a vector of length 100 with entries from a standard normal distribution
